59b0f8779e3a74dc4298a4c9f052190496e49a36
reiikennedy <mutuakennedy81@gmail.com>
2022-08-28 21:54:55 +0300

    feat: store auth tokens to localstorage

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Signup.css';
-import {Container, Grid, Item} from '@mui/material';
+import {Container, Grid} from '@mui/material';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 import p1 from '../assets/p1.jpg';
 import logo from '../assets/logo.png';
 
 const axios = require('axios');
-const baseURL = process.env.REACT_APP_BASEAPIURL || 'http://localhost:8000';
+const baseURL = process.env.REACT_APP_BASEAPIURL || 'http://127.0.0.1:8000';
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
@@ -27,7 +27,6 @@ class Signup extends React.Component {
             passwordErrors:""
         }
 
-        this.create = this.create.bind(this);
         this.handleChange =this.handleChange.bind(this);
     }
 
@@ -43,30 +42,40 @@ class Signup extends React.Component {
         });
     };
 
-    create(e){
+    registerUser = (e) => {
         e.preventDefault();
+        console.log(baseURL)
         this.state.email && this.state.firstname && this.state.lastname ? (
             this.state.email ? (
                 this.state.firstname && this.state.lastname ? (
-                    // axios({
-                    //     method:'post',
-                    //     url:`${baseURL}/apis/rest-auth/registration/`,
-                    //     data: {
-                    //         username: this.state.username,
-                    //         firstname:this.state.firstname,
-                    //         lastname:this.state.lastname,
-                    //         password1:this.state.password1,
-                    //         password2:this.state.password2,
-                    //         email:this.state.email
-                    //     }
-                    // })
-                    // .then(response => {
-                    //     console.log(response);
-                    // })
-                    // .catch(error =>{
-                    //     console.log(error);
-                    // })
-                    console.log(this.state.username)
+                    axios({
+                        method:'post',
+                        url:`${baseURL}/apis/rest-auth/registration/`,
+                        data: {
+                            username: this.state.username,
+                            firstname:this.state.firstname,
+                            lastname:this.state.lastname,
+                            password1:this.state.password1,
+                            password2:this.state.password2,
+                            email:this.state.email
+                        },
+                        config: {
+                            headers: { "Content-Type": "application/json" }
+                          }
+                    })
+                    .then(response => {
+
+                        console.log(response);
+                        // Store authorization tokens in local storage so we can use them to fire
+                        // subsequent Api requests for the user.
+                        // access token gives us access to server requests
+                        localStorage.setItem("access_token", response.data.access_tokens.access);
+                        // we will use the refresh token to get a new access token from the server when the initial one nears expiry
+                        localStorage.setItem("refresh_token", response.data.access_tokens.refresh);
+                    })
+                    .catch(error =>{
+                        console.log(error);
+                    })
 
                 ) : this.setState({fullNameErrors:"Please enter your first and last name"})
 
@@ -87,24 +96,24 @@ class Signup extends React.Component {
                                 <h2>Ready to get more done</h2>
                                 <p>Create an account to begin</p>
                             </div>
-                            <form>
+                            <form onSubmit={this.registerUser}>
                                 <input id='email' className={this.state.emailErrors ? 'fieldError' : ''} name='email' placeholder='jane.doe@email.com' onChange={(e)=> this.handleChange({email:e.target.value})} required></input>
                                 <span className='formErrors'>{this.state.emailErrors}</span>
                                 <div className="fullNames">
                                     <div className="fields">
-                                        <input id='firstname' className={this.state.fullNameErrors ? 'fieldError' : ''} name='firstname' placeholder='Firstname' onChange={(e)=> this.handleChange({email:e.target.value})} required></input>
+                                        <input id='firstname' className={this.state.fullNameErrors ? 'fieldError' : ''} name='firstname' placeholder='Firstname' onChange={(e)=> this.handleChange({firstname:e.target.value})} required></input>
                                         <span style={{width:'10px'}}></span>
-                                        <input id='lastname' className={this.state.fullNameErrors ? 'fieldError' : ''} name='lastname' placeholder='Lastname' onChange={(e)=> this.handleChange({email:e.target.value})} required></input>
+                                        <input id='lastname' className={this.state.fullNameErrors ? 'fieldError' : ''} name='lastname' placeholder='Lastname' onChange={(e)=> this.handleChange({lastname:e.target.value})} required></input>
                                     </div>
                                     <span className='formErrors'>{this.state.fullNameErrors}</span>
                                 </div>
                                 <div className='passwordField'>
-                                    <input id='password1' className={this.state.passwordErrors ? 'fieldError' : ''} name='password1' placeholder='Enter your password' onChange={(e)=> this.handleChange({password1:e.target.value})} required></input>
-                                    <input id='password2' className={this.state.passwordErrors ? 'fieldError' : ''} name='password2' placeholder='Confirm your password' onChange={(e)=> this.handleChange({password2:e.target.value})} required></input>
+                                    <input id='password1' type='password' className={this.state.passwordErrors ? 'fieldError' : ''} name='password1' placeholder='Enter your password' onChange={(e)=> this.handleChange({password1:e.target.value})} required></input>
+                                    <input id='password2' type='password' className={this.state.passwordErrors ? 'fieldError' : ''} name='password2' placeholder='Confirm your password' onChange={(e)=> this.handleChange({password2:e.target.value})} required></input>
                                     <span className='formErrors'>{this.state.passwordErrors}</span>
                                 </div>
                                 <span className='formErrors'>{this.state.authErrors}</span>
-                                <button onClick={(e)=> this.create(e)}>Sign up</button>
+                                <button type='submit'>Sign up</button>
                             </form>
                         </div>
                     </Grid>
