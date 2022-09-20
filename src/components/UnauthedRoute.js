512a9bf99805c71444afb0284dddd2a99960be99
reiikennedy <mutuakennedy81@gmail.com>
2022-09-21 01:43:06 +0300

    feat: add anauthed route

---
+++ src/components/UnauthedRoute.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Navigate, Route } from "react-router-dom";
+
+// component that handles where to take authenticated users
+const UnauthedRoute = ({ component: Component, loading, ...rest }) => {
+    // TO DO: check if token is about to expire
+  const isAuthed = Boolean(localStorage.getItem("access_token"));
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        loading ? (
+          <p>Loading...</p>
+        ) : !isAuthed ? (
+          <Component history={props.history} {...rest} />
+        ) : (
+          <Navigate
+            to={{
+              pathname: "/login",
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default UnauthedRoute;
