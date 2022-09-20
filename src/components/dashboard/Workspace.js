2bfd43080544b78bad83bc4b809ffb4ecc0e3eb5
reiikennedy <mutuakennedy81@gmail.com>
2022-09-21 01:43:16 +0300

    feat:

---
+++ src/components/dashboard/Workspace.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class Workspace extends React.Component {
+
+    return(){
+        return(
+            <p>My workspace</p>
+        )
+    }
+}
+
+export default Workspace;
