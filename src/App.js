3fdfedfd8983f55f2e68ac696b9c574c9d9aa174
reiikennedy <mutuakennedy81@gmail.com>
2022-09-21 01:42:05 +0300

    feat: Add public and private routes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import Header from './components/Header';
 import Signup from './components/Signup';
 import Login from './components/Login';
-import Workspace from './components/Workspace';
+import Workspace from './components/dashboard/Workspace';
 import AuthedRoute from './components/AuthedRoute';
+import UnauthedRoute from './components/UnauthedRoute';
 
 import {
   BrowserRouter,
@@ -15,12 +16,19 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} exact/>
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/workspace" element={<Workspace/>}/>
-        </Routes>
+          <Routes>
+              {/* no authentication required to access */}
+              <Route path="/" element={<App />} exact/>
+              {/* Only accessible if not authenticated */}
+              <Route path="/auth" element={<UnauthedRoute/>} exact>
+                <Route path="/auth/signup" element={<Signup/>}/>
+                <Route path="/auth/login" element={<Login/>}/>
+              </Route>
+              {/* Requires user to be authenticated */}
+              <Route path="/dashboard" element={<AuthedRoute />} exact>
+                <Route path="/dashboard/workspace" element={<Workspace/>}/>
+              </Route>
+          </Routes>
       </BrowserRouter>
       
     </div>
