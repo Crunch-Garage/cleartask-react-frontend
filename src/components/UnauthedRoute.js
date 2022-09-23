8f85b006483ee63404677290e5003cf379c8a360
reiikennedy <mutuakennedy81@gmail.com>
2022-09-23 12:58:10 +0300

    refactor: switch from function to class component

--- src/components/UnauthedRoute.js
+++ src/components/UnauthedRoute.js
@@ -1,28 +1,35 @@
 import React from "react";
-import { Navigate, Route } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 // component that handles where to take authenticated users
-const UnauthedRoute = ({ component: Component, loading, ...rest }) => {
-    // TO DO: check if token is about to expire
-  const isAuthed = Boolean(localStorage.getItem("access_token"));
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        loading ? (
-          <p>Loading...</p>
-        ) : !isAuthed ? (
-          <Component history={props.history} {...rest} />
-        ) : (
-          <Navigate
-            to={{
-              pathname: "/login",
-            }}
-          />
-        )
-      }
-    />
-  );
-};
+
+// const isAuthed = Boolean(localStorage.getItem("access_token"));
+
+class UnauthedRoute extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loggedOut:false
+    }
+  }
+
+  componentDidMount = () =>{
+    console.log('here')
+  }
+
+  render(){
+    return(
+      <div>
+        { this.state.loggedOut ? (
+            <Outlet/>
+            ) : (
+            <Navigate to='/dashboard/workspace' replace={true}/>
+            )
+        }
+      </div>
+    )
+  };
+}
+
 
 export default UnauthedRoute;
