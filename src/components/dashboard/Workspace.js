c85ba6461425afeec7224be6bd24612468690fca
reiikennedy <mutuakennedy81@gmail.com>
2022-09-25 14:23:59 +0300

    feat: add task overview cards

--- src/components/dashboard/Workspace.js
+++ src/components/dashboard/Workspace.js
@@ -2,7 +2,37 @@ import React from 'react';
 import Sidebar from './Sidebar';
 import Topnav from './Topnav';
 import "./Styles.css";
+import {Container, Grid, Card, CardContent} from '@mui/material';
+import { Icon } from '@iconify/react';
 
+
+const card = (
+    <React.Fragment>
+      <CardContent>
+        <div className='top'>
+            <div className='left'>
+                <div className='iconWrp'>
+                    <Icon className="icon" icon="mdi-light:star"/>
+                </div>
+                <h3>Tasks Completed</h3>
+            </div>
+            <span>08</span>
+        </div>
+        <div className="dvdr"></div>
+        <div className='middle'>
+            <div className='left'>
+
+            </div>
+            <div className='right'>
+                <h5>Tasks Completed</h5>
+                <p>from last week</p>
+            </div>
+        </div>
+      </CardContent>
+    </React.Fragment>
+  );
+
+  
 class Workspace extends React.Component {
     constructor(props){
         super(props)
@@ -27,6 +57,19 @@ class Workspace extends React.Component {
                     </div>
                     <div className="dashContent">
                         <p>My workspace</p>
+                        <Container className="dashCContainer">
+                            <Grid container spacing={3} className="dashCgrid">
+                                <Grid item xs={3} className="dashCGridItem">
+                                    <Card className="dashCCard" variant="outlined">{card}</Card>
+                                </Grid>
+                                <Grid item xs={3} className="dashCGridItem">
+                                    <Card className="dashCCard" variant="outlined">{card}</Card>
+                                </Grid>
+                                <Grid item xs={3} className="dashCGridItem">
+                                    <Card className="dashCCard" variant="outlined">{card}</Card>
+                                </Grid>
+                            </Grid>
+                        </Container>
                     </div>
                 </div>
             </div>
