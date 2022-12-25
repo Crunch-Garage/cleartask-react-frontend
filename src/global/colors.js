4867b82f81a20be8cbb77c9a9c339bfda151bf18
reiikennedy <mutuakennedy81@gmail.com>
2022-12-25 07:24:29 +0300

    feat: add global css color values

---
+++ src/global/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+    primary:"#EC058E", // theme primary
+    secondary:"0F0E0E", // theme secondary
+    primaryText:"#0f0e0e", //for texts top in the hierachy
+    secondaryText:"#747474", // for text second hierachy
+    primaryButton:"#EC058E", // primary button color
+    secondaryButton:"0F0E0E", // on hover or alternative to primary
+    background:"#F2F2F2"
+}
