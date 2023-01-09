79b31cecd8ac978e1818c71826071c379fa2e158
reiikennedy <mutuakennedy81@gmail.com>
2023-01-09 11:17:13 +0300

    fix: fix navlink useLocation error.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Header from './components/Header';
 import AppRouter from './components/Router';
 
 
@@ -8,7 +7,6 @@ function App() {
 
   return (
     <div className="App">
-      <Header/>
       <AppRouter/>
     </div>
   );
