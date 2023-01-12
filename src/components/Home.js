6dea73d597861641ff7e9a6df66fb9abf304422b
reiikennedy <mutuakennedy81@gmail.com>
2023-01-12 12:37:33 +0300

    feat: add homepage hero section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,26 @@
 import React from "react";
+import {Container, Grid} from '@mui/material';
 import { Link } from "react-router-dom";
 import "./Home.css";
 import Header from "./Header"
+
+
+const GetStartedInput = ()=>{
+    
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+    }
+
+    return(
+        <form onSubmit={handleSubmit} method="post">
+            <input type="email" placeholder="Enter your email"/>
+            <div className="cta">
+                <button type="submit">Get started</button>
+                <span>Free forever!<br/> No credit card</span>
+            </div>
+        </form>
+    )
+}
 
 class Home extends React.Component {
     constructor(props){
@@ -17,6 +36,18 @@ class Home extends React.Component {
             <Header/>
             <div className="homePage">
                 <div className="contBox">
+                    <Container>
+                        <Grid container spacing={3} className="heroGrid">
+                            <Grid item xs className="gridLeft gridText">
+                                <h2>All your projects & tasks in one place!</h2>
+                                <p>Get more productive. Get clear!</p>
+                                <GetStartedInput/>
+                            </Grid>
+                            <Grid item xs>
+                                
+                            </Grid>
+                        </Grid>
+                    </Container>
                     <h2>Welcome to Cleartask</h2>
                     <p>Clear more tasks off your to do list. Get more work done! It's that simple. </p>
 
