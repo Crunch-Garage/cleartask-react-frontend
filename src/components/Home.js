f532426d5504c8cfced85369796fe13213e34a0d
reiikennedy <mutuakennedy81@gmail.com>
2023-01-17 11:37:38 +0300

    refactor: rename css classes to BEM convention.

--- src/components/Home.js
+++ src/components/Home.js
@@ -31,9 +31,9 @@ const GetStartedInput = ()=>{
 
 const ReviewOverview =()=>{
     return(
-        < div className="ratingStats">
-            <div className="top">
-                <div className="ratingStars">
+        < div className="rating">
+            <div className="rating__stars">
+                <div className="rating__stars--box">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" strokeWidth={1.5} stroke="gold" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                     </svg>
@@ -52,7 +52,7 @@ const ReviewOverview =()=>{
                 </div>
                 <span>Based on 5000+ reviews on</span>
             </div>
-            <div className="reviewApps">
+            <div className="rating__review-apps">
                 <img src={getApp}/>
                 <img src={trusPilot}/>
                 <img src={capterra}/>
@@ -72,18 +72,18 @@ class Home extends React.Component {
     render(){
         return(
             <main className="main">
-                <div className="homePage">
-                    <section className="contBox">
-                        <Container>
-                            <Grid container spacing={3} className="heroGrid">
-                                <Grid item xs={5} className="gridLeft gridText">
+                <div className="hero">
+                    <section className="hero__section">
+                        <Container className="hero__container">
+                            <Grid container spacing={3} className="hero__grid">
+                                <Grid item xs={5} className="hero__grid--first">
                                     <h2>All your projects & tasks in one place!</h2>
                                     <p>Get more productive. Get clear!</p>
                                     <GetStartedInput/>
                                     <ReviewOverview/>
                                 </Grid>
-                                <Grid item xs={7}>
-                                    <div className="heroGraphic">
+                                <Grid item xs={7} className="hero__grid--last">
+                                    <div className="hero__grid-graphic">
                                         <img src={hr} alt="dashboard"/>
                                     </div>
                                 </Grid>
